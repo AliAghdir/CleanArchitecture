2c7a134655fbd2997ec3beecef0e98de2751a45e
Ali Taghi Aghdir <alitaghiaghdir@gmail.com>
2022-06-23 09:05:27 +0330

    Create UniversityDbContext

---
+++ src/Infra.Data/Context/UniversityDbContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using Domain.Models;
+
+namespace Infra.Data.Context
+{
+    public class UniversityDbContext : DbContext
+    {
+        public UniversityDbContext(DbContextOptions<UniversityDbContext> options)
+        :base(options)
+        {
+            
+        }
+
+        public DbSet<Course> Courses { get; set; }
+    }
+}
